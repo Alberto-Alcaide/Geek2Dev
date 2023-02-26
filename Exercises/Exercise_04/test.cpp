3523c0b21fe7e27ecd4df245186b5f5ac8c23b40
Alberto-Alcaide <alcaidevegaalberto@gmail.com>
2023-02-26 17:46:03 +0100

    Testing the color
    I have some doubts about the way that the colours should be represented

---
+++ Exercises/Exercise_04/test.cpp
@@ -0,0 +1,12 @@
+#include "Color.h"
+
+int main(){
+
+    std::cout << "Color rojo: " << Color::red() << "\n";
+    std::cout << "Color verde: " << Color::green() << "\n";
+    std::cout << "Color azul: " << Color::blue() << "\n";
+
+
+    return 0;
+}
+
