ea21e29c97c48c9f7fbac6c927aa98d0a1f99f12
Alberto-Alcaide <alcaidevegaalberto@gmail.com>
2023-03-02 21:28:48 +0100

    Created Exercise_06 but it doesn't compile

---
+++ Exercises/Exercise_06/chase_test.cpp
@@ -0,0 +1,44 @@
+#include "Engine2D.h"
+
+
+struct object{
+    Vec2D position;
+    int radius;
+    Color color;
+} player, enemy;
+
+int main(int argc, char *args[])
+{
+    // Initialize game engine
+    Engine2D engine(640, 400);
+    
+
+    // Entities initialization
+    player.radius = 5;
+    player.position = Vec2D(10,10);
+    player.color = Color::blue();
+    enemy.radius = 5;
+    enemy.position = Vec2D(400, 400);
+    enemy.color = Color::red();
+
+    while (engine.nextFrame())
+    {
+        engine.update();
+
+        // Code of the game
+        double dt=engine.getDeltaTime();
+        Vec2D direction = player.position-enemy.position;
+        enemy.position += direction * dt;
+        player.position = engine.GetMouse();
+        // int x, int y, int radius, Color color
+        Graphics::drawFillCircle(player.position.x, player.position.y,player.radius,player.color);
+        Graphics::drawFillCircle(player.position.x, player.position.y,player.radius,player.color);
+
+        engine.render();
+        
+    }
+    engine.~Engine2D();
+
+    return 0;
+
+}
