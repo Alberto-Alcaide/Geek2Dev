c4cab95531320f3732cf92174f016d79235da356
Pedrosa-c <79969364+Pedrosa-c@users.noreply.github.com>
2023-03-05 22:20:52 +0100

    creation of 04

--- Exercises/Exercise_04/04.cpp
+++ Exercises/Exercise_04/04.cpp
@@ -42,13 +42,5 @@ while (engine.nextFrame())
     }
     engine.~Engine2D();
 
-    std::cout << "Color rojo: " << Color::red() << "\n";
-    std::cout << "Color verde: " << Color::green() << "\n";
-    std::cout << "Color azul: " << Color::blue() << "\n";
-    std::cout << "Color Negro: " << Color::black() << "\n";
-
-    std::cout <<"White(255,255,255,1) in hexadecimal is: " << std::hex << Color::white().getColorRGB32() << std::endl;
-
     return 0;
 }
-
