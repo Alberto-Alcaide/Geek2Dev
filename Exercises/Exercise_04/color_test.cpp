3113eeb06509639ca203885cda9a63a73327ad1a
Pedrosa-c <79969364+Pedrosa-c@users.noreply.github.com>
2023-03-05 22:51:20 +0100

    Update color_test.cpp

--- Exercises/Exercise_04/color_test.cpp
+++ Exercises/Exercise_04/color_test.cpp
@@ -2,45 +2,8 @@
 #include <cmath>
 #include <string>
 
-struct object{
-    Vec2D position;
-    int radius;
-    int height;
-    int width;
-    Color color;
-} player, enemy;
-
 int main()
 {
-
-    // Initialize game engine
-    Engine2D engine(640, 400);
-    
-
-    // Entities initialization
-    player.height = 10;
-    player.width = 10;
-    player.position = Vec2D(10,10);
-    player.color = Color::blue();
-    enemy.height = 10;
-    enemy.width = 10;
-    enemy.position = Vec2D(400, 200);
-    enemy.color = Color::red();
-
-while (engine.nextFrame())
-    {
-        engine.update();
-
-        player.position = engine.mouse->position;
-
-        // int x, int y, int radius, Color color
-        Graphics::drawFillRect(player.position.x, player.position.y, player.width, player.height, player.color);
-        Graphics::drawRect(enemy.position.x, enemy.position.y, enemy.width, enemy.height, enemy.color);
-
-        engine.render();
-        
-    }
-    engine.~Engine2D();
 
     std::cout << "Color rojo: " << Color::red() << "\n";
     std::cout << "Color verde: " << Color::green() << "\n";
