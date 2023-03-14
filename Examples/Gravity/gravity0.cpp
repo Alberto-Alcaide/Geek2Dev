a90c308e783507050f51451981c03624809a47cb
Carlos Martinez <alu0101164809@ull.edu.es>
2023-03-14 12:28:09 +0100

    Fixed according to new naming

--- Examples/Gravity/gravity0.cpp
+++ Examples/Gravity/gravity0.cpp
@@ -4,9 +4,9 @@ int main(int argc, char *args[])
 {
     Engine2D engine(800,800);
 
-    const auto ball= engine.world.create();
-    engine.world.emplace<TransformComponent>(ball, Vec2D(400.0,400.0));
-    engine.world.emplace<KinematicComponent>(ball, Vec2D(0, -200), Vec2D(0, 100));
+    const auto ball= engine.world_.create();
+    engine.world_.emplace<TransformComponent>(ball, Vec2D(400.0,400.0));
+    engine.world_.emplace<KinematicComponent>(ball, Vec2D(0, -200), Vec2D(0, 100));
     int radius=40;
 
     while(engine.nextFrame())
@@ -15,7 +15,7 @@ int main(int argc, char *args[])
 
         // Code of example
         // The kinematic system is updating the positing behind the scenes
-        const auto transform = engine.world.get<TransformComponent>(ball);
+        const auto transform = engine.world_.get<TransformComponent>(ball);
         Graphics::drawFillCircle(transform.position.x, transform.position.y, radius, Color::blue());
 
         engine.render();
