c2d11fe006a73023a144a36c7050399e7b3793ee
Pedrosa-c <79969364+Pedrosa-c@users.noreply.github.com>
2023-03-25 14:19:36 +0100

    Update spring.cpp

--- Examples/spring-force/spring.cpp
+++ Examples/spring-force/spring.cpp
@@ -6,19 +6,19 @@ int main(int argc, char *args[]){
     int height = 800;
     Engine2D engine (width,height);
     Vec2D anchor = Vec2D(width/2,200);
-    float k = 20;
+    float k = 2;
     float restlenght = 200;
-    Vec2D gravity_force = Vec2D(0,150);
+    Vec2D gravity_force = Vec2D(0,50);
     Vec2D drag_force = Vec2D();
 
 
     const auto particle = engine.world_.create();
-    engine.world_.emplace<TransformComponent>(particle, Vec2D(width/2,600));
+    engine.world_.emplace<TransformComponent>(particle, Vec2D(width/2-50,500));
     engine.world_.emplace<KinematicsComponent>(particle);
     engine.world_.emplace<ParticleComponent>(particle, 1, 10, Color::red());
 
     auto& t = engine.world_.get<TransformComponent>(particle);
-    auto& kinematic = engine.world_.get<KinematicsComponent>(particle);
+    auto& kinematic = engine.world_.get<KinematicsComponent>(particle);s
     auto& p = engine.world_.get<ParticleComponent>(particle);
 
     
@@ -26,11 +26,11 @@ int main(int argc, char *args[]){
     {
         engine.update();
 
-        Vec2D spring_force = Force::Generate_Spring_Vector(t.position, anchor,restlenght, k);
-        drag_force=Force::Generate_Drag_Vector(0.1, kinematic.velocity);
+        Vec2D spring_force = Force::generateSpringVector(t.position, anchor,restlenght, k);
+        drag_force=Force::generateDragVector(0.001, kinematic.velocity);
         p.AddForce(spring_force);
-        //p.AddForce(gravity_force);
-        //p.AddForce(drag_force);
+        p.AddForce(gravity_force);
+        p.AddForce(drag_force);
         
         Graphics::drawFillRect(0, height/2, width, height, Color(14,91,158));
         Graphics::drawLine(t.position.x, t.position.y, anchor.x, anchor.y, Color::white());
