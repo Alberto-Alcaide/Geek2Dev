c4f72d1af6438f0582a83b96a28fbda7312d76e7
Pedrosa-c <79969364+Pedrosa-c@users.noreply.github.com>
2023-05-15 15:30:43 +0200

    Update main.cpp

--- final-project/main.cpp
+++ final-project/main.cpp
@@ -21,8 +21,8 @@ int main(int argc, char *args[])
 {
 
     //Vec2D weight = Vec2D(0.0,25.0);
-    int width=725;
-    int height=1000;
+    int width=600;
+    int height=800;
     int playerW=100;
     int playerH=25;
 
@@ -43,25 +43,25 @@ int main(int argc, char *args[])
     //Walls
         //celling
     const auto ceiling = engine.world.create();
-    engine.world.emplace<TransformComponent>(ceiling, Vec2D(0,100));
+    engine.world.emplace<TransformComponent>(ceiling, Vec2D(0,0));
     engine.world.emplace<KinematicsComponent>(ceiling);
     engine.world.emplace<NameGroupComponent>(ceiling, "celling", "map");
     engine.world.emplace<ColliderComponent>(ceiling, RectangleShape(width,25,Color::white(),true), true, false);
     engine.world.emplace<RigidBodyComponent>(ceiling, 0, RectangleShape(width, 25, Color::white(), true));
         //left wall 
     const auto l_wall = engine.world.create();
-    engine.world.emplace<TransformComponent>(l_wall, Vec2D(0,130));
+    engine.world.emplace<TransformComponent>(l_wall, Vec2D(0,26));
     engine.world.emplace<KinematicsComponent>(l_wall);
     engine.world.emplace<NameGroupComponent>(l_wall, "l_wall", "map");
-    engine.world.emplace<ColliderComponent>(l_wall, RectangleShape(25,height-250,Color::white(),true), true, false);
-    engine.world.emplace<RigidBodyComponent>(l_wall, 0, RectangleShape(25, height-250, Color::white(), true));
+    engine.world.emplace<ColliderComponent>(l_wall, RectangleShape(25,height-125,Color::white(),true), true, false);
+    engine.world.emplace<RigidBodyComponent>(l_wall, 0, RectangleShape(25, height-150, Color::white(), true));
         //right wall
     const auto r_wall = engine.world.create();
-    engine.world.emplace<TransformComponent>(r_wall, Vec2D(700,130));
+    engine.world.emplace<TransformComponent>(r_wall, Vec2D(575,26));
     engine.world.emplace<KinematicsComponent>(r_wall);
     engine.world.emplace<NameGroupComponent>(r_wall, "r_wall", "map");
-    engine.world.emplace<ColliderComponent>(r_wall, RectangleShape(50,height-250,Color::white(),true), true, false);
-    engine.world.emplace<RigidBodyComponent>(r_wall, 0, RectangleShape(50, height-250, Color::white(), true));
+    engine.world.emplace<ColliderComponent>(r_wall, RectangleShape(50,height-125,Color::white(),true), true, false);
+    engine.world.emplace<RigidBodyComponent>(r_wall, 0, RectangleShape(50, height-125, Color::white(), true));
 
 
 
@@ -117,8 +117,6 @@ int main(int argc, char *args[])
     {
         engine.update();
 
-
-
         // Check if all bricks are destroyed 
         auto view = engine.world.view<NameGroupComponent>();
         int i=0;
@@ -128,121 +126,8 @@ int main(int argc, char *args[])
                 i++;
             }
         }
-        /*engine.world.get<RigidBodyComponent>(ball).AddForce(weight);
-        auto forces = engine.world.get<RigidBodyComponent>(ball).GetForces();
-        std::cout << "Fuerzas: " << forces << std::endl;*/
-
-        //Graphics::drawGrid(gridSize);
-
-
-        /*
-        auto viewTag = engine.world.view<NameGroupComponent>();
-
-        for (auto entity : viewTag)
-        {
-            if (engine.world.get<NameGroupComponent>(entity).group=="map")
-            std::cout << "Position Walls" << engine.world.get<TransformComponent>(entity).position << std::endl;
-        }
-        */
-
-       /*
-        auto view = engine.world.view<NameGroupComponent, TransformComponent>();
-        for(auto entity : view)
-        {  
-            //const auto& nameGroup = view.get<NameGroupComponent>(entity);
-            //auto& transform = view.get<TransformComponent>(entity);
-
-
-            
-            if(nameGroup.group == "ball")
-            {
-                for(auto entity2 : view)
-                {
-                    const auto& nameGroup2 = view.get<NameGroupComponent>(entity2);
-                    Contact contact;
-
-                    
-                    if (nameGroup2.group=="brick")
-                    {
-
-                        
-                        if(Collision::IsColliding(entity,entity2,contact,engine.world))//abs(transform.position.x - (transform2.position.x+25)) < 50 && abs(transform.position.y - (transform2.position.y+25)) < 50)
-                        {    
-                            Log::Warning("Collision");
-                            OnCollider(entity,entity,contact,engine.world);
-                        }
-
-                    }
-
-                
-                }
-            }
-            
-
-        }*/
-
-
-        // draw players
-        //const auto player1TransformComp =  engine.world.get<TransformComponent>(player1);
-        /*Graphics::drawFillRect(
-            player1TransformComp.position.x, 
-            player1TransformComp.position.y, 
-            playerW,
-            playerH, 
-            Color::white()
-        );
-
-        // draw Walls
-        const auto ceilingTC = engine.world.get<Transfor0mComponent>(celling);
-        const auto l_wallTC = engine.world.get<TransformComponent>(l_wall);
-        const auto r_wallTC = engine.world.get<TransformComponent>(r_wall);
-        Graphics::drawFillRect(
-            celingTC.position.x, 
-            celingTC.position.y, 
-            width,
-            25, 
-            Color::white()
-        );
-        Graphics::drawFillRect(
-            l_wallTC.position.x, 
-            l_wallTC.position.y, 
-            25,
-            974, 
-            Color::white()
-        );
-        Graphics::drawFillRect(
-            r_wallTC.position.x, 
-            r_wallTC.position.y, 
-            25,
-            974, 
-            Color::white()
-        );
-
-        //draw briks
-        for(auto brick : bricks){
-            const auto transformComp = engine.world.get<TransformComponent>(brick);
-            //const auto colliderComp = engine.world.get<ColliderComponent>(brick);
-            //const auto& shape = colliderComp.shape;
-
-            Graphics::drawFillRect(
-                transformComp.position.x, 
-                transformComp.position.y, 
-                100,
-                25, 
-                Color::white()
-            );
-        }
-
-        //draw ball
-        auto ballTransform = engine.world.get<TransformComponent>(ball);
-        Graphics::drawFillRect(
-            ballTransform.position.x,
-            ballTransform.position.y,
-            25,
-            25,
-            Color::white()
-        );*/
-
+        
+        //check the level
         if(i==0){
             level++;
             Log::Error(to_string(level));
@@ -291,6 +176,11 @@ int main(int argc, char *args[])
                     //Log::Warning("terminé");
                     break;
                 }
+
+                //fix the enviorement
+
+
+
             }
 
         }
