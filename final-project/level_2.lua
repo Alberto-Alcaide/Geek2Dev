f8fbe266537f5695d8e691075c6109b9d22684bb
Alberto-Alcaide <alcaidevegaalberto@gmail.com>
2023-05-17 19:57:09 +0200

    Updated final-project
    Added a new level, sprites, backgrounds, new collisions and main works

--- final-project/level_2.lua
+++ final-project/level_2.lua
@@ -1,13 +1,13 @@
 -- Crear un vector de bricks en Lua
 local bricks = {
-    { x = 90, y = 200 },
-    { x = 200, y = 200 },
-    { x = 310, y = 200 },
-    { x = 420, y = 200 },
-    { x = 90, y = 310 },
-    { x = 200, y = 310 },
-    { x = 310, y = 310 },
-    { x = 420, y = 310 }
+    { x = 90, y = 200, color = "red" },
+    { x = 200, y = 200, color = "red" },
+    { x = 310, y = 200, color = "red" },
+    { x = 420, y = 200, color = "red" },
+    { x = 90, y = 310, color = "red" },
+    { x = 200, y = 310, color = "red" },
+    { x = 310, y = 310, color = "red" },
+    { x = 420, y = 310, color = "red" }
     -- Agregar más bricks según sea necesario
 }
 
